e19ce913a17efd13c84569515ba4760e9c8f4007
mariapaz <mpazcarvacho@gmail.com>
2022-01-30 01:55:38 -0300

    added model diagram

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,6 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :markers
 
-  # belongs_to :parent, class_name: "Category", foreign_key: :category_id, optional: :true
-
-  # has_many :subcategories, class_name: "Category", dependent: :destroy
-
   has_many :subcategories, class_name: "Category", foreign_key: "parent_id", dependent: :destroy
   
   belongs_to :parent, class_name: "Category", optional: :true
--- app/models/marker.rb
+++ app/models/marker.rb
@@ -1,4 +1,4 @@
 class Marker < ApplicationRecord
-  has_and_belongs_to_many :markers
+  has_and_belongs_to_many :categories
   has_and_belongs_to_many :types
 end
